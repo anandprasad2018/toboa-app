223125971e54ea7a8911cad56e5470f0b75b00e6
Anand Prasad <anandprasad2018@gmail.com>
2021-07-18 21:33:53 +0530

    Server.js Setting the Path crrt

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/toboa-app'));
+app.use(express.static('./dist/ToboaApp'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/toboa-app/'}),
+    res.sendFile('index.html', {root: 'dist/ToboaApp/'}),
 );
 
 // Start the app by listening on the default Heroku port
